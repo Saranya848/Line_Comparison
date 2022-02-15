2562829aba902be74755bc8e3eb41f38b3779d8d
SaranyaSiripurapu <saranyasripurapu@gmail.com>
2022-02-15 13:40:08 +0530

    To check Length of the Lines

--- src/com/comparison/LineComparison.java
+++ src/com/comparison/LineComparison.java
@@ -1,40 +1,40 @@
 package com.comparison;
 import java.util.Scanner;
 public class LineComparison {
-	 public static int getLine(Scanner c) {
-         System.out.println("Enter x1 Value");
-         int x1 = c.nextInt();
+	public static int getLength(Scanner c) {
+        System.out.println("Enter x1 value");
+        int x1 = c.nextInt();
 
-         System.out.println("Enter y1 Value");
-         int y1 = c.nextInt();
+        System.out.println("Enter y1 value");
+        int y1 = c.nextInt();
 
-         System.out.println("Enter x2 Value");
-         int x2 = c.nextInt();
+        System.out.println("Enter x2 value");
+        int x2 = c.nextInt();
 
-         System.out.println("Enter y2 Value");
-         int y2 = c.nextInt();
+        System.out.println("Enter y2 value");
+        int y2 = c.nextInt();
 
-         int output1 = (int) Math.pow((x2 - x1), 2);
-         int output2 = (int) Math.pow((y2 - y1), 2);
-         int result = (int) Math.sqrt(output1 + output2);
+        int output1 = (int) Math.pow((x2 - x1), 2);
+        int output2 = (int) Math.pow((y2 - y1), 2);
+        int result = (int) Math.sqrt(output1 + output2);
 
-         return result;      
- }
+        return result;
+}
 
- public static void main(String[] args) {
-         Scanner c = new Scanner(System.in);
-         System.out.println("Please enter Line1 values");
-         int length_line1 = getLine(c);
-         System.out.println("Please enter Line2 values");
-         int length_line2 = getLine(c);
-         String output1=String.valueOf(length_line1);
-         String output2=String.valueOf(length_line2);
-         if(output1.equals(output2))
+public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        System.out.println("Please enter values for line 1");
+        int length_line1 = getLength(c);
+        System.out.println("Please enter values for line 2");
+        int length_line2 = getLength(c);
+         if(length_line1 == length_line2)
              System.out.println("Both lines are equal");
+     else if(length_line1 > length_line2)
+             System.out.println("The line1 is greater than line2");
+     
+     else
+             System.out.println("The line2 is greater than line1");
 
-         else
-             System.out.println("Both lines are not equal");
-
-         c.close();
+        c.close();
 }
 }
